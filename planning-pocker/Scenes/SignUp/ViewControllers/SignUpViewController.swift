fa2ecf8f805ae2887a04cba084d816c6334d82d9
Slime <liem.nguyen@tpssoft.com>
2022-06-21 18:47:01 +0700

    Finished UI Sign Up, add App Icon, add Font Family into app

---
+++ planning-pocker/Scenes/SignUp/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpViewController.swift
+//  planning-pocker
+//
+//  Created by Nguyen Hong Liem on 6/21/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: - IBOutlets
+
+
+    // MARK: - Properties
+
+
+    // MARK: - Overrides
+
+
+    // MARK: - Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    // MARK: - Publics
+
+
+    // MARK: - Private
+
+
+    // MARK: - Actions
+
+
+    
+
+}
+
+// MARK: - extensions
+
+
+// MARK: - protocols
+
